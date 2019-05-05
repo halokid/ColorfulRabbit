0a56c60b09ae373a0dff592cff5e27d6eba7626c
r0x <r00txxyyzz@gmail.com>
2019-05-05 20:05:15 +0800

    common

--- common.go
+++ common.go
@@ -1,5 +1,7 @@
 package ColorfulRabbit
 
+import "strings"
+
 /**
  常规的函数
  */
@@ -14,6 +16,28 @@ func IndexOf(element string, data []string) (int) {
   }
   return -1 //not found.
 }
+
+
+// 判断map中key是否存在
+//func HasKey(key string, mapx map[string]interface{}) bool {
+func HasKey(key string, mapx map[string]map[string]interface{}) bool {
+  if _, ok := mapx[key]; ok {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+// 是否包含某字符串
+func Contain(checkStr string, patt string) bool {
+  if strings.Index(checkStr, patt) == -1 {
+    return false
+  } else {
+    return true
+  }
+}
+
 
 
 
