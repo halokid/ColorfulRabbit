8d92820c59104c18c92140965bc7044ee647b963
r0x <r00txxyyzz@gmail.com>
2019-05-06 19:58:45 +0800

    int to int64

--- type_trans.go
+++ type_trans.go
@@ -1,6 +1,9 @@
 package ColorfulRabbit
 
-import "github.com/bitly/go-simplejson"
+import (
+  "github.com/bitly/go-simplejson"
+  "strconv"
+)
 
 /**
 类型转换函数
@@ -13,4 +16,11 @@ func StrToJson(s string) *simplejson.Json {
 }
 
 
+// int转int64
+func IntToInt64(i int) int64 {
+  iStr := strconv.Itoa(i)   // int to string
+  iInt64, err := strconv.ParseInt(iStr, 10, 64)   // string to int64
+  CheckError(err)
+  return iInt64
+}
 
