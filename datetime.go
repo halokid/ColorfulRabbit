89e33b4931aa6fc234bb5348f5d1b07bcb46307a
r0x <r00txxyyzz@gmail.com>
2019-06-03 17:52:03 +0800

    datetime

---
+++ datetime.go
@@ -0,0 +1,13 @@
+package ColorfulRabbit
+
+import "time"
+
+/**
+  日期时间类
+ */
+
+func GetDateHour() string {
+  // get date, year:month:day:hour like 2019060115, 15 is hour
+  datehStr := time.Now().Format("2006010215")
+  return datehStr
+}
