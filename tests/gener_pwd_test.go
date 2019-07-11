7ab8503aa29c227e98940e1f1d720d2c9cc0508d
r0x <r00txxyyzz@gmail.com>
2019-07-11 14:54:39 +0800

    generate pwd

--- tests/gener_pwd_test.go
+++ tests/gener_pwd_test.go
@@ -12,8 +12,12 @@ func TestGenerPwd(t *testing.T) {
  //随机种子
   rand.Seed(time.Now().UnixNano())
   //test1()
-  passwd := ColorfulRabbit.GenerPwd("mix", 6)
+  passwd := ColorfulRabbit.GenerPwd("mix", 16)
   fmt.Println(passwd)
+
+
+  p2 := ColorfulRabbit.GenerPwd("mix", 16)
+  fmt.Println(p2)
   //fmt.Printf("length:%d charset:%s\n", length, charset)
 }
 
