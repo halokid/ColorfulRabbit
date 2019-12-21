5505d9aeee605dfcf878a375b59cccc5c6e453c6
mm jxx <r00txxyyzz@gmail.com>
2019-12-21 21:14:07 +0800

    os exec

--- sre.go
+++ sre.go
@@ -1,14 +1,20 @@
 package ColorfulRabbit
 
-import "os"
+import (
+  "bytes"
+  "fmt"
+  "io"
+  "os"
+  "os/exec"
+)
 
 /**
 系统运维类
  */
 
 
-// 判断文件是否存在
 func PathExists(path string) bool {
+  // 判断文件是否存在
   _, err := os.Stat(path)
   if err == nil {
     return true
@@ -18,6 +24,26 @@ func PathExists(path string) bool {
   }
   return false
 }
+
+
+func OsExecOut(cmdStr string)  {
+  // 执行系统命令,并且获取输出
+  fmt.Println("开始执行命令", cmdStr)
+  cmdx := exec.Command(cmdStr)
+  stdout, err := cmdx.StdoutPipe()
+  CheckFatal(err, " 执行命令", cmdStr, "输出失败")
+
+  var errbuf bytes.Buffer
+  cmdx.Stderr = &errbuf
+  err = cmdx.Start()
+  CheckError(err)
+  _, err = io.Copy(os.Stdout, stdout)
+  CheckError(err)
+  cmdx.Wait()
+  fmt.Println(errbuf.String())
+}
+
+
 
 
 
