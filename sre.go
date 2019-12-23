b3064e96085105205f51fe3a49b1d8b7e22da212
r0x <r00txxyyzz@gmail.com>
2019-12-23 19:09:25 +0800

    fix cmd trim

--- sre.go
+++ sre.go
@@ -31,14 +31,15 @@ func OsExecOut(cmdStr string)  {
   // 执行系统命令,并且获取输出
   fmt.Println("开始执行命令", cmdStr)
   cmdSpl := strings.Split(cmdStr, " ")
+  var cmdSplTr []string
   for i, v := range cmdSpl {
-    cmdSpl[i] = strings.Trim(v, " ")
+    cmdSplTr[i] = strings.Trim(v, " ")
   }
-  fmt.Println(cmdSpl)
-  cmdArgs := cmdSpl[1:]
+  fmt.Println(cmdSplTr)
+  cmdArgs := cmdSplTr[1:]
   fmt.Println(cmdArgs)
   //os.Exit(11)
-  cmdx := exec.Command(cmdSpl[0], cmdArgs...)
+  cmdx := exec.Command(cmdSplTr[0], cmdArgs...)
   stdout, err := cmdx.StdoutPipe()
   CheckFatal(err, " 执行命令", cmdStr, "输出失败")
 
