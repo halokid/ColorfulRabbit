aab8b3cd87a871c05f45cc5fa37f0871b204c1a4
r0x <r00txxyyzz@gmail.com>
2019-12-23 17:53:29 +0800

    sre exec command

--- sre.go
+++ sre.go
@@ -6,6 +6,7 @@ import (
   "io"
   "os"
   "os/exec"
+  "strings"
 )
 
 /**
@@ -29,7 +30,12 @@ func PathExists(path string) bool {
 func OsExecOut(cmdStr string)  {
   // 执行系统命令,并且获取输出
   fmt.Println("开始执行命令", cmdStr)
-  cmdx := exec.Command(cmdStr)
+  cmdSpl := strings.Split(cmdStr, " ")
+  //fmt.Println(cmdSpl)
+  cmdArgs := cmdSpl[1:]
+  //fmt.Println(cmdArgs)
+  //os.Exit(11)
+  cmdx := exec.Command(cmdSpl[0], cmdArgs...)
   stdout, err := cmdx.StdoutPipe()
   CheckFatal(err, " 执行命令", cmdStr, "输出失败")
 
