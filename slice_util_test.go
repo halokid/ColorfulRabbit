fefe40b6f7b90710b9e83a2ea0ed15f03cffa2e0
r0x <r00txxyyzz@gmail.com>
2021-03-03 14:22:19 +0800

    add test slice append

--- slice_util_test.go
+++ slice_util_test.go
@@ -7,3 +7,14 @@ func TestReverseSl(t *testing.T) {
   slx := ReverseSl(sl)
   t.Log(slx)
 }
+
+func TestComm(t *testing.T) {
+  s1 := []string{"a", "b", "c", "d", "e"}
+  s2 := []string{"a", "f", "k", "d", "b"}
+
+  var s3 []string
+  s3 = append(s1, s2...)
+  t.Log(s3)
+  s4 := RmRepeatSlcStr(s3)
+  t.Log(s4)
+}
