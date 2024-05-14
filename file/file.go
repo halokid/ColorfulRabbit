8719ce787876a0f6dc1325b6e000ea54dc79133e
hays <hays>
2024-05-14 17:50:07 +0800

    support async backup logger file

--- file/file.go
+++ file/file.go
@@ -2,6 +2,9 @@ package file
 
 import (
 	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -23,25 +26,57 @@ func ReadFile(filepath string) ([]string, error) {
   log.Println("1:", len(text))
 
   /*
-  var lines [][]string
-  for i, each_ln := range text {
-    log.Println("each line:", each_ln)
-    if i > 0 {
-      sl := strings.Split(each_ln, "|")
-      sl = utils.RemoveEmptyElements(sl)
+    var lines [][]string
+    for i, each_ln := range text {
+      log.Println("each line:", each_ln)
+      if i > 0 {
+        sl := strings.Split(each_ln, "|")
+        sl = utils.RemoveEmptyElements(sl)
 
-      for i, slItem := range sl {
-        sl[i] = strings.Replace(slItem, " ", "", -1)
-        sl[i] = strings.Replace(sl[i], "\t", "", -1)
-      }
+        for i, slItem := range sl {
+          sl[i] = strings.Replace(slItem, " ", "", -1)
+          sl[i] = strings.Replace(sl[i], "\t", "", -1)
+        }
 
-      if len(sl) > 1 {
-        lines = append(lines, sl)
+        if len(sl) > 1 {
+          lines = append(lines, sl)
+        }
       }
     }
-  }
   */
   return text, nil
 }
 
+func Copy(src, dst string) (int64, error) {
+  sourceFileStat, err := os.Stat(src)
+  if err != nil {
+    return 0, err
+  }
 
+  if !sourceFileStat.Mode().IsRegular() {
+    return 0, fmt.Errorf("%s is not a regular file", src)
+  }
+
+  source, err := os.Open(src)
+  if err != nil {
+    return 0, err
+  }
+  defer source.Close()
+
+  destination, err := os.Create(dst)
+  if err != nil {
+    return 0, err
+  }
+  defer destination.Close()
+
+  nBytes, err := io.Copy(destination, source)
+  return nBytes, err
+}
+
+func Empty(filePath string) error {
+  err := ioutil.WriteFile(filePath, []byte(""), 0744)
+	if err != nil {
+		panic(err)
+	}
+  return err
+}
--- logger/logger.go
+++ logger/logger.go
@@ -4,9 +4,12 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+
+	"github.com/halokid/ColorfulRabbit/file"
 )
 
 // var Logger *zap.SugaredLogger
@@ -37,46 +40,66 @@ func init() {
 
 var SugarLogger *zap.SugaredLogger
 
-func InitLogger(logLevelInit string) {
-    log.Println("-->>> Pkg logger init()")
-    writeSyncer := getLogWriter()
-    encoder := getEncoder()
-    
-    logLevel := zapcore.DebugLevel
-    switch logLevelInit {
-    case "info":
-        logLevel = zapcore.InfoLevel
-    case "warn":
-        logLevel = zapcore.WarnLevel
-    case "error":
-        logLevel = zapcore.ErrorLevel
-    }
+func InitLogger(logLevelInit, logFolder, logFile, logExt string) {
+  log.Println("-->>> Pkg logger init()")
+  writeSyncer := getLogWriter(logFolder, logFile, logExt)
+  encoder := getEncoder()
 
-    // core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
-    core := zapcore.NewCore(encoder, writeSyncer, logLevel)
+  logLevel := zapcore.DebugLevel
+  switch logLevelInit {
+  case "info":
+    logLevel = zapcore.InfoLevel
+  case "warn":
+    logLevel = zapcore.WarnLevel
+  case "error":
+    logLevel = zapcore.ErrorLevel
+  }
 
-    logger := zap.New(core)
-    SugarLogger = logger.Sugar()
+  // core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+  core := zapcore.NewCore(encoder, writeSyncer, logLevel)
+
+  logger := zap.New(core)
+  SugarLogger = logger.Sugar()
 }
 
 func getEncoder() zapcore.Encoder {
-    // return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
-    // return zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig())
+  // return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+  // return zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig())
 
-    config :=  zap.NewDevelopmentEncoderConfig()
-    config.EncodeLevel = zapcore.CapitalColorLevelEncoder
-    // return zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
-    return zapcore.NewConsoleEncoder(config)
+  config := zap.NewDevelopmentEncoderConfig()
+  config.EncodeLevel = zapcore.CapitalColorLevelEncoder
+  // return zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig())
+  return zapcore.NewConsoleEncoder(config)
 }
 
-func getLogWriter() zapcore.WriteSyncer {
-    // file, _ := os.Create("./test.log")
-    file, _ := os.OpenFile("./log/runtime.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
-    ws := io.MultiWriter(file, os.Stdout)   // both console and file
-    // return zapcore.AddSync(file)
-    return zapcore.AddSync(ws)
+func getLogWriter(logFolder, logFile, logExt string) zapcore.WriteSyncer {
+  // file, _ := os.Create("./test.log")
+  // file, _ := os.OpenFile("./log/runtime.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
+  file, _ := os.OpenFile(logFolder + logFile + logExt, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
+  ws := io.MultiWriter(file, os.Stdout) // both console and file
+
+  go backupLog(logFolder, logFile, logExt)
+
+  // return zapcore.AddSync(file)
+  return zapcore.AddSync(ws)
 }
 
+func backupLog(logFolder, logFile, logExt string) {
+  ticker := time.NewTicker( 60 * time.Second)
+  for {
+    select {
+    case <-ticker.C:
+      backupFile := time.Now().Format("2006-01-02_15-04-05")
+      // copy log file
+      src := logFolder + logFile + logExt
+      dst := logFolder + logFile + "-" + backupFile + logExt
+      file.Copy(src, dst)
 
-
-
+      // empty log file
+      err := file.Empty(src)
+      if err != nil {
+        log.Printf("Logger backupLog err -->>> %+v", err)
+      }
+    }
+  }
+}
